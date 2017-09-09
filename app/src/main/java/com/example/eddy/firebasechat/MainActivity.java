c930b8b4c6e41656aaa1841e0e1bcb9f0bf78421
edto <edto@users.noreply.github.com>
2017-09-09 15:02:24 +1000

    main layout on

--- app/src/main/java/com/example/eddy/firebasechat/MainActivity.java
+++ app/src/main/java/com/example/eddy/firebasechat/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.eddy.firebasechat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.analytics.FirebaseAnalytics;
-
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
