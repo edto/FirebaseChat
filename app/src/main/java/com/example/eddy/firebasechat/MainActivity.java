4e89556aca0d56ffd048b56f1ad94b21921fa6bb
edto <ed.to@me.com>
2017-09-12 00:33:43 +1000

    stuff

--- app/src/main/java/com/example/eddy/firebasechat/MainActivity.java
+++ app/src/main/java/com/example/eddy/firebasechat/MainActivity.java
@@ -22,48 +22,51 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private static final int SIGN_IN_REQUEST_CODE = 10;
     private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+        // No user logged in
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+        {
             // Start sign in/sign up activity
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),
-                    SIGN_IN_REQUEST_CODE);
-        } else {
-            // User is already signed in. Therefore, display
-            // a welcome Toast
-            Toast.makeText(this,
-                    "Welcome " + FirebaseAuth.getInstance()
-                            .getCurrentUser()
-                            .getDisplayName(),
-                    Toast.LENGTH_LONG)
-                    .show();
+                                   SIGN_IN_REQUEST_CODE);
+        }
+        else
+        {
+            // User is already logged in
+            Toast.makeText(this,"Welcome " + FirebaseAuth.getInstance().getCurrentUser()
+                            .getDisplayName(), Toast.LENGTH_LONG).show();
 
-            // Load chat room contents
             displayChatMessages();
         }
 
-        FloatingActionButton fab =
-                (FloatingActionButton)findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 EditText input = (EditText)findViewById(R.id.input);
 
                 // Read the input field and push a new instance
                 // of ChatMessage to the Firebase database
+                // ChatMessage contains a msg string and display name
                 FirebaseDatabase.getInstance()
                         .getReference()
                         .push()
@@ -73,6 +76,16 @@ public class MainActivity extends AppCompatActivity {
                                         .getDisplayName())
                         );
 
+                Toast.makeText(MainActivity.this, "Posted msg as: " +
+                        FirebaseAuth.getInstance()
+                                .getCurrentUser()
+                                .getDisplayName()
+                        + " from " +
+                        FirebaseAuth.getInstance()
+                                    .getCurrentUser()
+                                    .getEmail()
+                        , Toast.LENGTH_LONG).show();
+
                 // Clear the input
                 input.setText("");
             }
@@ -80,12 +93,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
-    private void displayChatMessages() {
+    private void displayChatMessages()
+    {
         ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
         adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
@@ -111,23 +126,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == SIGN_IN_REQUEST_CODE) {
-            if(resultCode == RESULT_OK) {
+        if(requestCode == SIGN_IN_REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
                 Toast.makeText(this,
-                        "Successfully signed in. Welcome!",
-                        Toast.LENGTH_LONG)
-                        .show();
-                displayChatMessages();
-            } else {
-                Toast.makeText(this,
-                        "We couldn't sign you in. Please try again later.",
-                        Toast.LENGTH_LONG)
-                        .show();
+                        "Successfully signed in.", Toast.LENGTH_LONG).show();
 
-                // Close the app
+                displayChatMessages();
+            }
+            else
+            {
+                Toast.makeText(this, "Error when signing in. Please try again later.",
+                        Toast.LENGTH_LONG).show();
+
+                // Close app
                 finish();
             }
         }
@@ -135,18 +152,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.menu_sign_out) {
-            AuthUI.getInstance().signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // If sign out button is tapped
+        if(item.getItemId() == R.id.menu_sign_out)
+        {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>()
+                    {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(MainActivity.this,
-                                    "You have been signed out.",
-                                    Toast.LENGTH_LONG)
-                                    .show();
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            Toast.makeText(MainActivity.this, "You have been signed out.",
+                                    Toast.LENGTH_LONG).show();
 
-                            // Close activity
+                            // Close app
                             finish();
                         }
                     });
